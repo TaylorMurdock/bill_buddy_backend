20294d12ef8af68036d54a4681f4fd96e6929186
Taylor Murdock <tjmurd4545@gmail.com>
2023-09-23 16:48:45 -0400

    backend work with module

--- billbuddy/models.py
+++ billbuddy/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class Bills(models.Model):
     name_of_subscription = models.CharField(max_length=100)
-    subscription_image_url = models.ImageField(upload_to='images/')
+    # find a way todo url links.... image db with all sub pictures in that db for the user to select from....... have it so when the sub name matches image name in db auto does it
+    # lets wait for imgs
+    subscription_image_url = models.CharField(max_length=400)
     bill_date = models.CharField(max_length=100)
     subscription_price = models.CharField(max_length=100)
