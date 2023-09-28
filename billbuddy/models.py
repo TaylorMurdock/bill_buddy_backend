4158c0e86a7f9d64b9139f636e8cfa7912769471
Taylor Murdock <tjmurd4545@gmail.com>
2023-09-27 21:46:19 -0400

    bill date model updated

--- billbuddy/models.py
+++ billbuddy/models.py
@@ -8,5 +8,6 @@ class Bills(models.Model):
     # find a way todo url links.... image db with all sub pictures in that db for the user to select from....... have it so when the sub name matches image name in db auto does it
     # lets wait for imgs
     subscription_image_url = models.CharField(max_length=400)
-    bill_date = models.CharField(max_length=100)
+    bill_date = models.DateField()
+    # bill_date = models.CharField(max_length=100)
     subscription_price = models.CharField(max_length=100)
